9374585422b471e2fcfb0569ad03bdae630b54c1
foulhadj <foulhadj@fr.scc.com>
2025-02-26 17:15:15 +0100

    EXP : foulhadj

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,41 @@
-# Use a Python base image
 FROM pytorch/pytorch:1.13.1-cuda11.6-cudnn8-runtime
 
-# Set the working directory inside the container
-RUN mkdir -p /opt/app/src
 WORKDIR /opt/app
 
-# Copy the requirements file into the container
 COPY requirements.txt .
+RUN pip install -r requirements.txt
 
-# Update pip and install the dependencies
-RUN pip install --upgrade pip && \
-    pip install --no-cache-dir -r requirements.txt
-
-# Copy the application code into the container
-COPY finetuning.py .
+COPY finetuning.py
 COPY input_data ./input_data
 
 # Set environment variables
 ENV WANDB_DISABLED=true
 
-# Définir la commande par défaut pour exécuter l'application
 CMD ["python", "finetuning.py", "--report_to", "none"]
+
+# # Use a Python base image
+# FROM pytorch/pytorch:1.13.1-cuda11.6-cudnn8-runtime
+
+# # Set the working directory inside the container
+# RUN mkdir -p /opt/app/src
+# WORKDIR /opt/app
+
+# # Copy the requirements file into the container
+# COPY requirements.txt .
+
+# # Update pip and install the dependencies
+# RUN pip install --upgrade pip && \
+#     pip install --no-cache-dir -r requirements.txt
+
+# # Copy the application code into the container
+# COPY finetuning.py .
+# COPY input_data ./input_data
+
+# # Set environment variables
+# ENV WANDB_DISABLED=true
+
+# # Définir la commande par défaut pour exécuter l'application
+# CMD ["python", "finetuning.py", "--report_to", "none"]
 
 
 # # Use a Python base image
--- finetuning_legacy.py
+++ finetuning_legacy.py
@@ -6,9 +6,16 @@ from sentence_transformers import SentenceTransformer, InputExample
 from sentence_transformers.losses import MatryoshkaLoss, MultipleNegativesRankingLoss
 from sentence_transformers.evaluation import InformationRetrievalEvaluator
 from torch.utils.data import DataLoader
+import torch
 
+import warnings
+warnings.filterwarnings("ignore", message="Using the `WANDB_DISABLED` environment variable is deprecated")
 
-# Configuration
+## Vérification du device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+
+## Configuration
 nest_asyncio.apply()
 login(token=os.getenv('HUGGING_TOKEN'), add_to_git_credential=False)
 os.environ["WANDB_DISABLED"] = "true"
@@ -18,7 +25,7 @@ EPOCHS = 5
 
 # Load model from the hub
 MODEL_ID = "Snowflake/snowflake-arctic-embed-m-v2.0"
-model = SentenceTransformer(MODEL_ID, trust_remote_code=True)
+model = SentenceTransformer(MODEL_ID, trust_remote_code=True).to(device)
 
 # Load training dataset
 with open("input_data/training_dataset.jsonl", "r") as f:
@@ -33,7 +40,13 @@ examples = [
     for query_id, query in queries.items()
 ]
 
-loader = DataLoader(examples, batch_size=BATCH_SIZE)
+def collate_fn(batch):
+    """Transforme une liste d'InputExample en un format utilisable et déplace les données sur le bon device"""
+    texts = [example.texts for example in batch]  # Liste de paires (query, document)
+    # Déplacer les inputs vers le device ici
+    return {"texts": [t.to(device) if isinstance(t, torch.Tensor) else t for t in texts]}
+
+loader = DataLoader(examples, batch_size=BATCH_SIZE, drop_last=True, collate_fn=collate_fn)
 
 # Load validation dataset
 with open("input_data/val_dataset.jsonl", "r") as f:
@@ -45,18 +58,25 @@ val_relevant_docs = val_dataset['relevant_contexts']
 
 # Define loss function
 matryoshka_dimensions = [768, 512, 256, 128, 64]
-inner_train_loss = MultipleNegativesRankingLoss(model)
+inner_train_loss = MultipleNegativesRankingLoss(model).to(device)
 train_loss = MatryoshkaLoss(
     model,
     inner_train_loss,
     matryoshka_dims=matryoshka_dimensions,
-)
+).to(device)
+
+#train_loss = train_loss.to("cpu")
+
+print(f"Model device: {next(model.parameters()).device}")
+print(f"Loss device: {next(train_loss.parameters()).device}")
 
 # Define evaluator
 evaluator = InformationRetrievalEvaluator(val_queries, val_corpus, val_relevant_docs)
 
 # Training configuration
 warmup_steps = int(len(loader) * EPOCHS * 0.1)
+
+# Training
 model.fit(
     train_objectives=[(loader, train_loss)],
     epochs=EPOCHS,
@@ -65,6 +85,7 @@ model.fit(
     show_progress_bar=True,
     evaluator=evaluator,
     evaluation_steps=50,
+    #use_amp=True,  # mixed precision pour éviter certains conflits
 )
 
 # Save the model to Hugging Face Hub
